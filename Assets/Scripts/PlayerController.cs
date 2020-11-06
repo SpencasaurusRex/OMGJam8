a3cbc9f0c71b82145b4a3d3ea5071c5233de1275
SpencasaurusRex <SpencasaurusRex@gmail.com>
2020-11-06 06:46:53 -0500

    Get player animating

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public Transform Feet;
     public float CoyoteTime = 0.2f;
     
+    Animator anim;
     float gravity;
     Rigidbody2D rb;
     bool grounded;
@@ -17,7 +18,9 @@ public class PlayerController : MonoBehaviour
     
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        
         groundDetection = new Collider2D[4];
         groundMask = LayerMask.GetMask("Ground");
         gravity = rb.gravityScale;
@@ -25,6 +28,8 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        int state = 0;
+        
         grounded = Physics2D.OverlapBoxNonAlloc(Feet.position, Vector2.one * 0.05f, 0, groundDetection, groundMask) > 0;
         if (grounded)
         {
@@ -37,6 +42,15 @@ public class PlayerController : MonoBehaviour
         
         var targetVel = rb.velocity;
         targetVel.x = Input.GetAxisRaw("Horizontal") * Speed;
+
+        if (Mathf.Abs(targetVel.x) > 0)
+        {
+            state = 1;
+            print(state);
+        }
+        
+        if (targetVel.x > 0) transform.localScale = new Vector3(1, 1, 1);
+        if (targetVel.x < 0) transform.localScale = new Vector3(-1, 1, 1);
         
         if (lastTouchingGround <= CoyoteTime && Input.GetKey(KeyCode.Space))
         {
@@ -44,17 +58,20 @@ public class PlayerController : MonoBehaviour
             targetVel.y = JumpVelocity;
         }
         
-        if (targetVel.y < 0)
+        if (targetVel.y < -0.05f)
         {
             rb.gravityScale = gravity * 1.5f;
-            
+            state = 3;
         }
-        if (targetVel.y >= 0)
+        if (targetVel.y > 0.05f)
         {
             rb.gravityScale = gravity;
+            state = 2;
         }
         
         rb.velocity = targetVel;
+
+        anim.SetInteger("State", state);
     }
 
     void OnDrawGizmosSelected()
